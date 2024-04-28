c11637c7624d243afdc03d2e55cdd41bfdd80e7e
Cede555 <conorhalford@hotmail.co.uk>
2024-04-29 00:05:00 +0100

    Added KittyMeow

---
+++ kittymeow/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+  std::string tImg = { " /\\_/\\\n" };
+  std::string mImg = { "( o.o )\n" };
+  std::string bImg = { " > ^ <\n" };
+  std::string spacing = { "   " };
+  std::string inp = { "" };
+  std::string ts = { "" };
+
+  inp += argv[1];
+  for(size_t i = 0; i < inp.length(); i++)
+  {
+    ts += " "; 
+  }
+  
+  std::cout << spacing << ts << spacing << tImg << spacing << argv[1] << spacing << mImg << spacing << ts << spacing << bImg;
+
+}
